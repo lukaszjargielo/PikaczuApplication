9d6e46152619685b22361c032a153d86db3f4416
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-27 10:35:42 +0200

    Outline of classes: Author and Book

---
+++ pikaczu/src/main/java/model/Author.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Author {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private List<Book> books;
+
+}
---
+++ pikaczu/src/main/java/model/Book.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Book {
+    private Integer id;
+    private String title;
+    private Author author;
+    private String ean;
+}
