8fd962c9943d2cb413ed5010ef596d5ad8418829
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-27 09:04:04 +0200

    Skeleton of application

---
+++ pikaczu/src/main/java/pl/futurejava/pikaczu/PikaczuApplication.java
@@ -0,0 +1,13 @@
+package pl.futurejava.pikaczu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PikaczuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PikaczuApplication.class, args);
+	}
+
+}
