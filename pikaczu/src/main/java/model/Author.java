86f6f2564502e3174a0d94106dfe6470d1b35242
lukaszjargielo <l.jargielo88@gmail.com>
2025-06-27 10:51:50 +0200

    Added annotations to classes

--- pikaczu/src/main/java/model/Author.java
+++ pikaczu/src/main/java/model/Author.java
@@ -1,9 +1,29 @@
 package model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String firstName;
+
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
     private List<Book> books;
 
 }
--- pikaczu/src/main/java/model/Book.java
+++ pikaczu/src/main/java/model/Book.java
@@ -1,8 +1,36 @@
 package model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+@Entity
+
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String title;
-    private Author author;
+
+    @ManyToMany
+    @JoinTable(
+            name = "books_authors",
+            joinColumns =
+            @JoinColumn(name = "book_id"),
+            inverseJoinColumns =
+            @JoinColumn(name = "author_id")
+    )
+    private List<Author> authors;
+
     private String ean;
 }
